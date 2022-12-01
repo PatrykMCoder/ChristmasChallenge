d38ef5ce5939ea6f1d9e0f77d748304a8d719aa6
PatrykMCoder <patrykmarciszewski3@gmail.com>
2022-12-01 12:16:39 +0100

    Fix DayOne

--- challenge/DayOne.java
+++ challenge/DayOne.java
@@ -3,21 +3,27 @@ public class DayOne
     public static void main(String[] args) {
         double[] xPoints = new double[]{1, 4, 11};
         double[] yPoints = new double[]{1, 5, 5};
-        double usedMagicFuel = 0.0d;
+        double usedMagicFuel = 0.0;
         double distance;
-        double x1;
-        double y1;
+        double x1, x2;
+        double y1, y2;
+        double stops = xPoints.length - 1; // we have one less stop than all points: first point is start.
+        
+        double xStart = xPoints[0];
+        double yStart = yPoints[0];
 
-        for (int i = 0; i < xPoints.length - 1; i++) {
+        double distanceBeetweenStartAndFirstStop = Math.sqrt(Math.pow(xPoints[1] - xStart, 2) +  Math.pow(yPoints[1] - yStart, 2));
+
+        for (int i = 1; i < stops; i++) {
             x1 = xPoints[i];
             y1 = yPoints[i];
-            double x2 = xPoints[i + 1];
-            double y2 = yPoints[i + 1];
+            
+            x2 = xPoints[i + 1];
+            y2 = yPoints[i + 1];
 
             double distanceX1X2 = Math.pow(x2 - x1, 2);
             double distanceY1Y2 = Math.pow(y2 - y1, 2);
-            distance = Math.sqrt(distanceX1X2 + distanceY1Y2);
-            System.out.println(distance);
+            distance =  distanceBeetweenStartAndFirstStop +  Math.sqrt(distanceX1X2 + distanceY1Y2);
             usedMagicFuel = (distance * 20) / 10;
         }
 
