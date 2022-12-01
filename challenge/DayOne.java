d49ef3d10b91c4a69b29bdff01ad613ddcd018bd
PatrykMCoder <patrykmarciszewski3@gmail.com>
2022-12-01 12:25:43 +0100

    Clean up code for DayOne

--- challenge/DayOne.java
+++ challenge/DayOne.java
@@ -4,7 +4,6 @@ public class DayOne
         double[] xPoints = new double[]{1, 4, 11};
         double[] yPoints = new double[]{1, 5, 5};
         double usedMagicFuel = 0.0;
-        double distance;
         double x1, x2;
         double y1, y2;
         double stops = xPoints.length - 1; // we have one less stop than all points: first point is start.
@@ -12,21 +11,25 @@ public class DayOne
         double xStart = xPoints[0];
         double yStart = yPoints[0];
 
-        double distanceBeetweenStartAndFirstStop = Math.sqrt(Math.pow(xPoints[1] - xStart, 2) +  Math.pow(yPoints[1] - yStart, 2));
-
+        double distanceBeetweenStartAndFirstStop = DayOne.calculateDistance(xPoints[1], xStart, yPoints[1], yStart);
+        double distance = distanceBeetweenStartAndFirstStop;
         for (int i = 1; i < stops; i++) {
+            System.out.println(i);
             x1 = xPoints[i];
             y1 = yPoints[i];
             
             x2 = xPoints[i + 1];
             y2 = yPoints[i + 1];
 
-            double distanceX1X2 = Math.pow(x2 - x1, 2);
-            double distanceY1Y2 = Math.pow(y2 - y1, 2);
-            distance =  distanceBeetweenStartAndFirstStop +  Math.sqrt(distanceX1X2 + distanceY1Y2);
+            distance += DayOne.calculateDistance(x1, x2, y1, y2);
             usedMagicFuel = (distance * 20) / 10;
         }
 
         System.out.println("Used magic fuel: " + usedMagicFuel);
+    }
+
+
+    private static double calculateDistance(double x1, double x2, double y1, double y2) { 
+        return Math.sqrt(Math.pow(x2 - x1, 2) +  Math.pow(y2 - y1, 2));
     }
 }
