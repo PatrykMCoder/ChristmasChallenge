0c35d0c26ec5686835f956712086840cbe00d75f
PatrykMCoder <patrykmarciszewski3@gmail.com>
2022-12-04 18:44:13 +0100

    DayFour

---
+++ challenge/DayFour.java
@@ -0,0 +1,53 @@
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DayFour {
+    public static void main(String[] args) {
+        List<String> ideas = List.of(
+            "Parfum", "Socks", "Sweather", "Cup",
+            "Hat", "Tea", "Coffee", "Clock", "Bag",
+            "Book", "Wallet", "Cream", "Earrings");
+            ArrayList<BinaryWord> binaryWords = new ArrayList<>();
+        
+        ideas.forEach(idea -> {
+            BinaryWord binaryWord = new BinaryWord();
+
+            try {
+                byte[] b = idea.getBytes("UTF8");
+                for(int i = 0; i < b.length; i++) {
+                    binaryWord.putBinaryLetter(Integer.toBinaryString(b[i]));
+                }
+                binaryWords.add(binaryWord);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        });
+
+        // Display binary words
+        binaryWords.forEach(bw -> {
+            bw.getBinaryLetters().forEach(bl -> {
+                System.out.print(bl + " ");
+            });
+            System.out.println("");
+        });
+    }
+}
+
+class BinaryWord {
+    private ArrayList<String> binaryLetters;
+
+    public BinaryWord() {
+        binaryLetters = new ArrayList<>();
+    }
+
+    public void putBinaryLetter(String bin) {
+        binaryLetters.add(bin);
+    }
+
+    public ArrayList<String> getBinaryLetters() {
+        return binaryLetters;
+    }
+
+}
