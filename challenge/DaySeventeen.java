3f3a7779ae8eba63040f63055cb693c7477a518b
PatrykMCoder <patrykmarciszewski3@gmail.com>
2022-12-18 00:22:23 +0100

    Fix DaySeventeen

--- challenge/DaySeventeen.java
+++ challenge/DaySeventeen.java
@@ -1,17 +1,17 @@
 
 public class DaySeventeen {
     public static void main(String[] args) {
-        int height = 60;
+        int height = 20;
         int radius = height / 2;
         int posX = height;
         int posY = height;
+        System.out.println(posX + radius);
         char[] chars = {'#', '*'};
         for (int i = 0; i < posX + radius; i++) {
             for (int j = 0; j < posY + radius; j++) {
-                int xSquare = (i - posX) * (i - posX); 
-                int ySquare = (j - posY) * (j - posY); 
-
-                if (xSquare + ySquare - radius <= radius) {
+                int xSquare = (int)Math.pow(i - posX, 2); 
+                int ySquare = (int)Math.pow(j - posY, 2); 
+                if (xSquare + ySquare <= radius) {
                     int random = (int)((Math.random() * (chars.length - 0)) + 0);
                     System.out.print(chars[random]);
                 } else {
